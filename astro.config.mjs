95997322c9825d5f624f255d9d32c039cded0584
Roman Garanzuay <romanmgaranzuay@gmail.com>
2026-08-10 01:38:03 -0500

    Social Card Optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://manosoul.com',
+
   vite: {
     plugins: [tailwindcss()]
   },
